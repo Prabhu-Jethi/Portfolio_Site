b6b1b2168fc3dc21707b7e50ac3c49d343d917a3
Prabhu-Jethi <prabhujethi9@gmail.com>
2025-06-26 20:37:33 +0530

    Second commit

--- scripts/seedProjects.js
+++ scripts/seedProjects.js
@@ -11,13 +11,13 @@ const sampleProjects = [
     title: "MERN Todo App",
     description: "Manage tasks using MongoDB, Express, React, and Node.js.",
     tech: ["MongoDB", "Express", "React", "Node.js"],
-    github: "https://github.com/yourusername/todo-app",
+    github: "https://github.com/Prabhu-Jethi/To-do-App",
   },
   {
     title: "Portfolio Website",
     description: "My portfolio site with React, Bootstrap, and animations.",
     tech: ["React", "Bootstrap", "AOS", "Node.js"],
-    github: "https://github.com/yourusername/portfolio",
+    github: "https://github.com/Prabhu-Jethi/Portfolio_Site",
   },
 ];
 
